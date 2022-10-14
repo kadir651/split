f2982976030e1e21e4db2e5db386f21dd1a9bd72
kadir651 <115784747+kadir651@users.noreply.github.com>
2022-10-14 11:06:27 +0200

    Add files via upload

---
+++ build/precache-manifest.767544f7fbdcca9c0ec367e68a6ce631.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6e022e2fab1fc202164",
+    "url": "/static/css/main.c9d1e1a3.chunk.css"
+  },
+  {
+    "revision": "e6e022e2fab1fc202164",
+    "url": "/static/js/main.e6e022e2.chunk.js"
+  },
+  {
+    "revision": "cfc64a697c8ba560db09",
+    "url": "/static/css/1.fa96d6ff.chunk.css"
+  },
+  {
+    "revision": "cfc64a697c8ba560db09",
+    "url": "/static/js/1.cfc64a69.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13e446099e4a45f5516b124ab2d2b3c6",
+    "url": "/static/media/ssbnw-1.13e44609.png"
+  },
+  {
+    "revision": "b7afbb35cab0c7ea1c89ca276f472e5d",
+    "url": "/index.html"
+  }
+];
